a39e67142e9affcf22823e68739fef7fbf708283
Paul Fell <swellpf@gmail.com>
2021-08-09 10:06:20 -0400

    cleanup

--- frequency_counter.js
+++ frequency_counter.js
@@ -3,14 +3,16 @@
 
 
 function validAnagram(str1, str2){
-     if (str1.length !== str2.length) {
+    if (str1.length !== str2.length) {
          return false;
-     }
-     let frequencyCounter1 = {};
-     let frequencyCounter2 = {};
-     for (let val of str1) {
+    }
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    
+    for (let val of str1) {
          frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
      }
+    
      for (let val of str2) {
          frequencyCounter2[val] = (frequencyCounter2[val] || 0 ) + 1
      }
@@ -23,6 +25,6 @@ function validAnagram(str1, str2){
             return false;
         }
     }
-      return true
+    return true
       
 }
