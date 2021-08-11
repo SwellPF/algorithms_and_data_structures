d423fdb352f1cc9883295df17d1158b07e930d1f
Paul Fell <swellpf@gmail.com>
2021-08-10 20:52:30 -0400

    wrote recursive palindrome function

---
+++ recursion_is_palindrome.js
@@ -0,0 +1,9 @@
+// Write a function that accepts a string and returns true if the string
+// is a valid palindrome. Otherwise returns false.
+
+function isPalindrome(string){
+    if(string.length === 1) return true;
+    if(string.length === 2) return str[0] === str[1];
+    if (string[0] === string.slice(-1)) return isPalindrome(string.slice(1,-1));
+    return false;
+  }
