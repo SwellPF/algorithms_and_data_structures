2819d8083ea1722aaf802f844f7c1e8c0d2e0b1a
Paul Fell <swellpf@gmail.com>
2021-08-10 21:04:27 -0400

    wrote recursive capitalize first letter algorithm

---
+++ recursion_capitalize_first.js
@@ -0,0 +1,12 @@
+// Write a function that accepts an array of strings and capitalizes the first letter of
+// each string in the array.
+
+function capitalizeFirst (array) {
+    if (array.length === 1) {
+      return [array[0][0].toUpperCase() + array[0].substr(1)];
+    }
+    const result = capitalizeFirst(array.slice(0, -1));
+    const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+    result.push(string);
+    return result;
+  }
