35d7bd90888d467d3a69d86a67d299c1184c12da
Paul Fell <swellpf@gmail.com>
2021-08-14 13:50:30 -0400

    initial commit for dll insert and remove functions

---
+++ dll_insert_and_remove.js
@@ -0,0 +1,85 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        let count, current;
+        if(index <= this.length/2) {
+            count = 0;
+            current = this.head;
+            while (count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(current !== index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+    
+    push(val) {
+        var node = new Node(val);
+        if (this.head === null) {
+            this.head = node;
+            this.tail = this.head;
+            this.length++;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+            this.length++;
+         }
+        return this;
+    }
+    insert(index, val){
+        if(index < 0 || index >= this.length) return false;
+        if(index === 0) return !!unshift(index, val);
+        if(index === this.length) return !!push(index, val);
+        let foundNode = get(index-1);
+        
+        
+    }
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return shift(index);
+        if(index === this.length - 1) return pop(index);
+        let removeNode = this.get(index);
+        removeNode.prev.next = removeNode.next;
+        removeNode.next.prev = removeNode.prev;
+        removeNode.next = null;
+        removeNode.prev = null;
+        this.length--;
+        return removeNode;
+    }
+    push(val){
+        var node = new Node(val);
+        if (this.head === null) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
+    }
+}
