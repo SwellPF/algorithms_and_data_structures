d673eb203f472223387094ccbf85e4ead4d4210f
Paul Fell <swellpf@gmail.com>
2021-08-26 20:44:41 -0400

    wrote Set function for SLL

---
+++ Exercises/SLL_Set function.js
@@ -0,0 +1,46 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;      
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    
+    get(index){
+       if (index < 0 || index >= this.length) return null;
+       let counter = 0;
+       let current = this.head;
+       while(counter !== index){
+           counter ++;
+           current = current.next
+       }
+       return current.val;
+    }
+
+    set(index, val){
+        let foundNode = this.get(index);
+        if(foundNode){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+}
