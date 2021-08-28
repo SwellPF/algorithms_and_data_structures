fe492bdbbd4094417ec0418dc50b17126c14572f
Paul Fell <swellpf@gmail.com>
2021-08-28 16:53:39 -0400

    working on logic for end marker

--- Exercises/divide_and_conquer_sorted_frequency.js
+++ Exercises/divide_and_conquer_sorted_frequency.js
@@ -1,33 +1,27 @@
 function sortedFrequency(array, target){
     let start = 0;
     let end = array.length;
-    let foundMin=999;
-    let foundMax=999;
+    let foundMin=0;
+    let foundMax=array.length;
     console.log(`Target: ${target}`);
     while(start <= end){
         let middle = Math.floor((start + end)/2);
-        console.log(start, end, middle);
         if(array[middle] < target) start = middle +1;
-        if(array[middle] > target) foundMax = end;
         if(array[middle] >= target) {
             end = middle -1;
         }
-        if(array[middle] == target && array[middle-1] != target) foundMin = start;
+        if(array[middle] == target && array[middle-1] != target) foundMin = middle;
     }
-    end = foundMax; start = foundMin;
-    console.log(`foundMin: ${foundMin}   foundMax: ${foundMax}`);
+    end = array.length-1; 
+    start = foundMin;
     while(start <= end){
         let middle = Math.floor((start + end)/2);
-        console.log(start, end, middle);
-        if(array[middle] < target) start = middle +1;
-        if(array[middle] >= target) end = middle -1;
-        if(array[middle] === target && array[middle+1] !== target){
-            foundMax = end;
+        if(array[middle] <= target) start = middle +1;
+        if(array[middle] == target && array[end] == target){
+            foundMax = middle;
             return (foundMax - foundMin + 1);
         }
-    return -1;
+        if(array[middle] > target) end = middle -1; 
     }
+    return -1;
 }
-      //         0 1 2 3 4 5 6 7          
-sortedFrequency([1,1,2,2,2,2,2,3], 2)   //5
-                           
