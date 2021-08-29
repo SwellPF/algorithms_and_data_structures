ebace420b62ebb1c01152fd071b9c7248a809725
Paul Fell <swellpf@gmail.com>
2021-08-29 17:40:26 -0400

    corrected logic

--- Exercises/divide_and_conquer_sorted_frequency.js
+++ Exercises/divide_and_conquer_sorted_frequency.js
@@ -1,8 +1,8 @@
 function sortedFrequency(array, target){
     let start = 0;
     let end = array.length;
-    let foundMin = 0;
-    let foundMax = 0;
+    let foundMin;
+    let foundMax;
     if(target > array[array.length-1] || target < array[0]) return -1;
     while(start <= end){
         let middle = Math.floor((start + end)/2);
@@ -14,11 +14,15 @@ function sortedFrequency(array, target){
     start = foundMin;
     while(start <= end){
         let middle = Math.floor((start + end)/2);
-        console.log(`Evaluating... startIdx: ${start}, middleIdx: ${middle}, endIdx: ${end}, FoundMin: ${foundMin}, FoundMax: ${foundMax}`)
+        // console.log(`Evaluating... startIdx: ${start}, middleIdx: ${middle}, endIdx: ${end}, FoundMin: ${foundMin}, FoundMax: ${foundMax}`)
         if(array[middle] <= target) start = middle +1;
         if(array[middle] > target) end = middle - 1;
         if(array[middle] == target) foundMax = middle;
     }
-        console.log(`Success!!!! From: ${foundMin} To: ${foundMax}`);
-        return (foundMax - foundMin + 1);
+    if (foundMax){
+            console.log(`Success!!!! From: ${foundMin} To: ${foundMax}`);
+            return (foundMax - foundMin + 1);
+        } else {
+            return -1;
+        }
 }
