c18ecac4b890ab4d51a703dbd12fdff4c42989c7
Paul Fell <swellpf@gmail.com>
2021-08-28 21:42:46 -0400

    corrected end point logic and edge cases

--- Exercises/divide_and_conquer_sorted_frequency.js
+++ Exercises/divide_and_conquer_sorted_frequency.js
@@ -1,27 +1,24 @@
 function sortedFrequency(array, target){
     let start = 0;
     let end = array.length;
-    let foundMin=0;
-    let foundMax=array.length;
-    console.log(`Target: ${target}`);
+    let foundMin = 0;
+    let foundMax = 0;
+    if(target > array[array.length-1] || target < array[0]) return -1;
     while(start <= end){
         let middle = Math.floor((start + end)/2);
         if(array[middle] < target) start = middle +1;
-        if(array[middle] >= target) {
-            end = middle -1;
-        }
-        if(array[middle] == target && array[middle-1] != target) foundMin = middle;
+        if(array[middle] >= target) end = middle -1;
+        if(array[middle] == target) foundMin = middle;
     }
     end = array.length-1; 
     start = foundMin;
     while(start <= end){
         let middle = Math.floor((start + end)/2);
+        console.log(`Evaluating... startIdx: ${start}, middleIdx: ${middle}, endIdx: ${end}, FoundMin: ${foundMin}, FoundMax: ${foundMax}`)
         if(array[middle] <= target) start = middle +1;
-        if(array[middle] == target && array[end] == target){
-            foundMax = middle;
-            return (foundMax - foundMin + 1);
-        }
-        if(array[middle] > target) end = middle -1; 
+        if(array[middle] > target) end = middle - 1;
+        if(array[middle] == target) foundMax = middle;
     }
-    return -1;
+        console.log(`Success!!!! From: ${foundMin} To: ${foundMax}`);
+        return (foundMax - foundMin + 1);
 }
